efa0be62fdac210f447a0e346d46fa452ab55f53
jairo <jairo7martins@gmail.com>
2021-11-03 19:01:44 -0300

    changer validator to server

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.engine('html', require('hbs').__express);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
-app.use(cors())
+
+// app.use(cors())
 
 app.get('/', (req, res)=>{
     res.render('index')
@@ -23,16 +24,27 @@ app.get('/ia', (req, res)=>{
 
 app.post('/random', (req, res) =>{
 
-    const escolhaIA = ['Pedra', 'Papel', 'Tesoura']
+    const escolhaJogador = req.body.a;
+    const opcoes = ['Pedra', 'Papel', 'Tesoura']
 
-    const  min = Math.ceil(0)
+    const min = Math.ceil(0)
     const max = Math.floor(2)
-    const numero = Math.floor(Math.random() * (max - min + 1)) + min;
+    const escolhaIA = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    res.json({
-        id:numero,
-        opt:escolhaIA[numero]
-    })
+    var data = {}
+
+    data['opt'] = opcoes[escolhaIA]
+
+    if(escolhaIA == escolhaJogador){
+        data['resultado']= 'empate' 
+    }else
+    if((escolhaJogador - escolhaIA == -2)|| (escolhaJogador - escolhaIA == 1)){
+        data['resultado']= 'vitoria'  
+    }else{
+        data['resultado']= 'derrota'  
+    }
+
+    res.json(data)
 })
 
 app.listen( port,()=>{
